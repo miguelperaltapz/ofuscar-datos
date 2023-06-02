dca18cb59805ea3d150354d6a4be0bd23097cd48
miguelperaltapz <miguelperaltapz@gmail.com>
2023-06-02 10:55:11 -0600

    Hardcodeo. No se ofuscan todas las coincidencias, y solo en algunas paginas

--- opt.js
+++ opt.js
@@ -1,38 +1,100 @@
+function buildSaveArray() {
+    var saveArray = [];
+    var element = document.querySelectorAll('.keyword-row');
+    for(var i = 0; i < element.length; i++) {
+        var obj = {};
+        obj.keyword = element[i].querySelector('.keyword input').value;
+        obj.type = element[i].querySelector('.type select').value;
+        obj.replace = element[i].querySelector('.replace input').value;
+        saveArray.push(obj);
+    }
+
+    save_options(saveArray);
+}
+
 // Saves options to chrome.storage
-const saveOptions = () => {
-  
+function save_options(saveArray) {
+
     chrome.storage.sync.set({ 
-         
-    }, () => {
+        keywordsArray: saveArray
+    }, function() {
         // Update status to let user know options were saved.
-        const status = document.getElementById('status');
+        var status = document.getElementById('status');
         status.textContent = 'Options saved.';
-        setTimeout(() => {
+        setTimeout(function() {
           status.textContent = '';
         }, 750);
     });
-};
+}
   
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-const restoreOptions = () => {
+function restoreOptions() {
     chrome.storage.sync.get({ 
         keywordsArray: []
-    }, (items) => {
+    }, function(items) {
+
         buildOptDisplay(items.keywordsArray);
     });
-};
+}
+
+function buildOptDisplay(items) {
+    if(items.length == 0) {
+        document.querySelector('.add-keyword').click();
+    }
+    
+    for (var i = 0; i < items.length; i++) {
+        if(typeof items[i] === 'object') {
+            createRowWithOptions(items[i], i);
+        }
+    }
+}
+
+function createRowWithOptions(obj, int = 0) {
+    
+    var keywordRow = document.querySelector('.keyword-row').innerHTML;
+    // document.querySelector('.keywords-holder').innerHTML += keywordRow;
+
+    // Remove first item
+    if(typeof document.querySelector('.keyword-row').dataset.id === 'undefined') {
+        document.querySelector('.keyword-row').remove();
+    }
+    var newRow = document.createElement('div');
+    newRow.className = 'keyword-row';
+    var timestamp = (Date.now() + int);
+    newRow.dataset.id = timestamp;
+    newRow.innerHTML = keywordRow;
+    document.querySelector('.keywords-holder').appendChild(newRow);
+
+    var newEle = document.querySelector('.keywords-holder .keyword-row[data-id="' + timestamp + '"]');
+    newEle.querySelector('.keyword input').value = obj.keyword;
+    newEle.querySelector('.type select').value = obj.type;
+    if(obj.type == '1') {
+        newEle.querySelector('.replace').style.display = 'block';
+        newEle.querySelector('.replace input').value = obj.replace;
+    }
+    newEle.querySelector('.type select').addEventListener('change', function(e){
+        console.log(e);
+        var element = e.target;
+        var parent = element.parentNode.parentNode;
+        if(element.value == '1') {
+            parent.querySelector('.replace').style.display = 'block';
+        } else {
+            parent.querySelector('.replace').style.display = 'none';
+        }
+    });
+
+}
 
 
 // Listener to add keyword button
 document.querySelector('.add-keyword').addEventListener('click', function() {
-    var keywordRow = document.querySelector('.keyword-row').innerHTML;
-    // document.querySelector('.keywords-holder').innerHTML += keywordRow;
-    var newRow = document.createElement('div');
-    newRow.className = 'keyword-row';
-    newRow.innerHTML = keywordRow;
-    document.querySelector('.keywords-holder').appendChild(newRow);
+    var obj = {};
+    obj.keyword = 'example';
+    obj.type = '1';
+    obj.replace = 'string';
+    createRowWithOptions(obj);
 });
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save').addEventListener('click', saveOptions);
+document.getElementById('save').addEventListener('click', buildSaveArray);
--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-console.log("***********CONTENT SCRIPT***********");
+// alert("*******");
+// console.log("*********" + document.getElementsByClassName('sc-1kj39fw-0 bPflYs')[0].innerHTML);
 
-// var email = document.getElementsByClassName('email-text')[0].innerHTML;
+chrome.storage.sync.get({
+    keywordsArray: []
+  }, function(items) {
+    replacePageWords(items.keywordsArray);
+  });
+  function replacePageWords(keywordsArray){
+    //...
+    for (var i = 0; i < keywordsArray.length; i++) {
+      replaceWord(keywordsArray[i]);
+    }
+  }
+  function replaceWord(obj){
+    console.log('replace checks for obj', obj);
+    var allElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, a, div, caption, span, td');
+    for (var i = 0; i < allElements.length; i++) {
+      if(allElements[i].innerText.toLowerCase().includes(obj.keyword.toLowerCase())){
+        if(obj.type == '0'){
+          //remove
+          allElements[i].innerHTML = allElements[i].innerHTML.replace(obj.keyword, '');
+        }else if(obj.type == '1'){
+          //replace
+          allElements[i].innerHTML = allElements[i].innerHTML.replace(obj.keyword, obj.replace);
+        }
+      }
+    }
+  }
+
+// var email = document.getElementsByClassName('sc-1kj39fw-0 bPflYs').innerHTML;
 
 // function obfuscateEmail(email) {
 //     var splitEmail = email.split('@');
